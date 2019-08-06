9ae7d0a7de5a320a372a79784167074ad974aa14
Vlad Hybalo <2pi15b.gybalo@gmail.com>
2019-08-06 08:38:50 +0300

    base functional

--- src/components/BaseButton/BaseButton.js
+++ src/components/BaseButton/BaseButton.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './BaseButton.css';
 
 function BaseButton({children, handleClick}) {
-    console.dir(handleClick);
-    console.dir(children);
     return (
         <button className='base-button' onClick={handleClick}>{children}</button>
     )
